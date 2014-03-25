1e62be1edc63eee10f78eca04b304c399d3e40a8
Spencer Buyansky <sbuyansky@gmail.com>
2014-03-25 17:47:33 -0500

    initial insertRecord and HeapFile constructor finished

--- heapfile.C
+++ heapfile.C
@@ -51,17 +51,18 @@ HeapFile::HeapFile(const string & fileName, Status& returnStatus)
     // open the file and read in the header page and the first data page
     if ((status = db.openFile(fileName, filePtr)) == OK)
     {
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+		if((status = filePtr->getFirstPage(headerPageNo)) != OK){
+			returnStatus = status;
+			return;
+		}
+		filePtr->readPage(headerPageNo, (Page *) headerPage);
+		hdrDirtyFlag = true;
+
+		bufMgr->readPage(filePtr,headerPage->firstPage,curPage);
+		curPageNo = headerPage->firstPage;
+		curDirtyFlag = true;
+		curRec = NULLRID;
+
     }
     else
     {
@@ -357,18 +358,18 @@ const Status InsertFileScan::insertRecord(const Record & rec, RID& outRid)
         return INVALIDRECLEN;
     }
 
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+	//check if there is space in the page
+	if((status = curPage->insertRecord(rec, outRid)) != OK){
+		//if there is not space in the page, allocate a new page
+		if((status = filePtr->allocatePage(newPageNo)) == OK){
+			//read the page in to newPage
+			if((status = filePtr->readPage(newPageNo, newPage)) == OK){
+				//insert the record into the newly allocated page
+				status = newPage->insertRecord(rec, outRid);
+			}
+		}
+	}
+	return status;
 }
 
 
